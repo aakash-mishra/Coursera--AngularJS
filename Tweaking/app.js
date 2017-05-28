350f4c3b87a7a48fb8fccfcd742647274a884729
aakash <aakash21696@gmail.com>
2017-05-29 02:24:31 +0530

    Almost

--- Tweaking/app.js
+++ Tweaking/app.js
@@ -2,20 +2,20 @@
 
   'use strict';
   angular.module("PasswordStrength",[])
-.controller("PasswordStrengthController",function($scope){
-  $scope.strength="weak";
-  $scope.pass="";
-  $scope.calculateStrength = function(){
-    if($scope.pass.length<5)
-      $scope.strength="weak";
-    else if($scope.pass.length>5 && $scope.pass.length<8)
-      $scope.strength="strong";
-    else
-      $scope.strength="very strong!!";
-  };
+.controller("PasswordStrengthController",PasswordStrengthController);
 
-
-});
+function PasswordStrengthController($scope){
+    $scope.strength="weak";
+    $scope.pass="";
+    $scope.calculateStrength = function(){
+      if($scope.pass.length<=5)
+        $scope.strength="weak";
+      else if($scope.pass.length>5 && $scope.pass.length<8)
+        $scope.strength="strong";
+      else
+        $scope.strength="very strong!!";
+    };
+}
 
 
 
